84a21eba9a4ddea9edb06eed90aac2549979ff29
ruizsepulvedamaicolandres-code <ruizsepulvedamaicolandres@gmail.com>
2026-03-04 14:58:45 -0500

    Se subio las animaciones
    Faltan los Sprite de lado, pero ya tenemos la base para animar todas las mecanicas

--- Assets/Srcripts/Player.cs
+++ Assets/Srcripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     [SerializeField] private float dashDuration;
     private float dashTimer;
     private bool isDashing = false;
+    public Animator animator;
 
   
 
@@ -38,9 +39,14 @@ public class Player : MonoBehaviour
 
     private void Move ()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
+        float velocidadX = Input.GetAxis("Horizontal")*Time.deltaTime*speed;
+        float velocidadY = Input.GetAxis("Vertical")*Time.deltaTime*speed;
+        animator.SetFloat("Movement", velocidadX*speed);//Las condiciones del movimiento 
+        Vector2 posicion = transform.position;
+        transform.position = new Vector2(velocidadX + posicion.x,velocidadY + posicion.y);
+       /* movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        movement = movement.normalized;
+        movement = movement.normalized; */
     }
     
     private void Dash()
